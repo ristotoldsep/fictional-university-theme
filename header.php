7a9594a34abadf9f67713c90cfdab7fef5d9d1da
Risto Tõldsep <r.toldsep@gmail.com>
2020-12-11 00:50:27 +0200

    Added front-page.php and updated /blogs functionality

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
+              <li><a href="<?php echo site_url('/blog')?>">Blog</a></li>
             </ul>
             <!-- Outputting a dynamic WP connected menu -->
             <?php 
