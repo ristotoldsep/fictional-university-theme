a9e3e2685f5b25d9e4bf93ba75973f781d23bc11
Risto Tõldsep <r.toldsep@gmail.com>
2021-01-08 13:02:34 +0200

    Improved search-route function & custom REST API route

--- includes/search-route.php
+++ includes/search-route.php
@@ -12,31 +12,72 @@
         //3rd argument = array that describes how we want to manage this field
         register_rest_route('university/v1', 'search', array( //university/v1/search/...
             'methods' => WP_REST_SERVER::READABLE, //means = 'GET', but just in case if some web hosts don't know what GET is
-            'callback' => 'universitySearchResults' //calls this function
+            'callback' => 'universitySearchResults' //calls this function, and passes along $data about the current request that someone is sending
         ));
     }
 
     //To create our own custom JSON
-    function universitySearchResults() { //WP automatically converts PHP into JSON data (don't need to write JSON syntax)
-       $professors = new WP_Query(array(
-           'post_type' => 'professor'
+    function universitySearchResults($data) { //WP automatically converts PHP into JSON data!!!! (don't need to write JSON syntax)
+       
+        //Main query to REST API custom URL
+        $mainQuery = new WP_Query(array(
+           'post_type' => array('post', 'page', 'program', 'professor', 'event', 'institution'),
+           's' => sanitize_text_field($data['term'])  //s = search |$data = array that WordPress puts together and within this is the term someone searched for
        ));
        
        // return $professors->posts; //LOOPS through the professor object and posts array automatically (and outputs 10 by default)
        //Since we don't use 90% of the JSON data we get back, we are populating the JSON with only the data we want !!
        
-       $professorResults = array();
+       //POPULATING this array with certain post types
+       $results = array(
+           'generalInfo' => array(),
+           'professors' => array(),
+           'programs' => array(),
+           'events' => array(),
+           'institutions' => array()
+       );
 
-       while($professors->have_posts()) { //Loop length = number of professors
-         $professors->the_post();
-
-         array_push($professorResults, array( //Output the combined array of objects in JSON
-            'title' => get_the_title(),
-            'permalink' => get_the_permalink()
-         ));
+    
+       while($mainQuery->have_posts()) { //Loop length = number of professors
+            $mainQuery->the_post();
+            
+            if (get_post_type() == 'post' OR get_post_type() == 'page') {
+                array_push($results['generalInfo'], array( //Output the combined array of objects in JSON
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'ID' => get_the_ID()
+                ));   
+            }
+            if (get_post_type() == 'professor') {
+                array_push($results['professors'], array( //Output the combined array of objects in JSON
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'ID' => get_the_ID()
+                ));   
+            }
+            if (get_post_type() == 'program') {
+                array_push($results['programs'], array( //Output the combined array of objects in JSON
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'ID' => get_the_ID()
+                ));   
+            }
+            if (get_post_type() == 'event') {
+                array_push($results['events'], array( //Output the combined array of objects in JSON
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'ID' => get_the_ID()
+                ));   
+            }
+            if (get_post_type() == 'institution') {
+                array_push($results['institutions'], array( //Output the combined array of objects in JSON
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'ID' => get_the_ID()
+                ));   
+            }
        }
-
-       return $professorResults;
+       return $results; //Return the associative array with JSON data
     }
 
 
