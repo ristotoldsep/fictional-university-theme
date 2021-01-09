d41193d31e8892d5a6bab911352a1d32b4a7019f
Risto Tõldsep <r.toldsep@gmail.com>
2021-01-09 14:06:48 +0200

    Created custom query on My notes page to output notes data from DB

--- page-my-notes.php
+++ page-my-notes.php
@@ -1,4 +1,14 @@
-<?php get_header();  //POWERS ABOUT US PAGE!!!!!!
+<?php //POWERS ABOUT US PAGE!!!!!!
+
+//REDIRECTING THE USER if he somehow gets to this URL and is not logged in! - MUST come before get_header()
+    if (!is_user_logged_in()) {
+        wp_redirect(esc_url(site_url('/')));
+        exit; //Stop using server resources
+    }
+
+get_header();  
+
+    
 
     while(have_posts()) {
         the_post(); 
@@ -16,7 +26,31 @@
 
   <div class="container container--narrow page-section">
 
-    Custom code will go here!
+    <ul class="min-list link-list" id="my-notes">
+        <?php  
+            //Creating a custom query object to retrieve notes from WP database (mySQL)
+            $userNotes = new WP_Query(array(
+                'post_type' => 'note',
+                'posts_per_page' => -1,
+                'author' => get_current_user_id()
+            ));
+
+            while($userNotes->have_posts()) {
+                $userNotes->the_post(); ?>
+                
+                <li>
+                    <input class="note-title-field" type="text" value="<?php echo esc_attr(get_the_title()); ?>">
+
+                    <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+
+                    <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+
+                    <textarea class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); //esc for security, strip tagas to remove wordpress html tags and comments! ?></textarea>
+                </li>
+        
+            <?php } ?>
+        
+    </ul>
 
   </div>
         
